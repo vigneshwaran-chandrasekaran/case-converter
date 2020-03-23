2bf886b34daf72cae288d50b7e8fd1821872c5c2
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-03-23 22:33:34 +0530

    basic text area form added

--- src/pages/CaseConverter.js
+++ src/pages/CaseConverter.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export function CaseConverter() {
-	return <div>CaseConverter</div>;
+	const [value, setValue] = useState('');
+
+	function handleChange(event) {
+		setValue(event.target.value);
+	}
+
+	function handleSubmit(event) {
+		alert('An essay was submitted: ' + value);
+		event.preventDefault();
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label>
+					Essay:
+					<textarea value={value} onChange={handleChange} />
+				</label>
+				<input type="submit" value="Submit" />
+			</form>
+		</div>
+	);
 }
