95a5447f2941c08cbadbcdc67de0a7bdf9847656
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-03-24 20:52:44 +0530

    text style changed

--- src/pages/CaseConverter.js
+++ src/pages/CaseConverter.js
@@ -1,35 +1,34 @@
 import React, { useState } from 'react';
-import { Input } from 'antd';
+import { Input, Row, Col, Typography, Divider } from 'antd';
 import { camelCase } from 'lodash';
 
 const { TextArea } = Input;
+const { Text } = Typography;
 
 export function CaseConverter() {
-	const [value, setValue] = useState('');
+	const [value, setValue] = useState('sample text');
 
 	function handleChange(event) {
 		setValue(event.target.value);
 	}
 
-	function handleSubmit(event) {
-		alert('An essay was submitted: ' + value);
-		event.preventDefault();
-	}
-
 	return (
 		<div>
-			<TextArea rows={4} value={value} onChange={handleChange} />
-			<form onSubmit={handleSubmit}>
-				<label>
-					<textarea value={value} onChange={handleChange} />
-				</label>
-				<div>
-					<input type="submit" value="Submit" />
-				</div>
-				<p>{value.toUpperCase()}</p>
-				<p>{value.toLowerCase()}</p>
-				<p>{camelCase(value)}</p>
-			</form>
+			<Row type="flex" justify="center">
+				<Col span={20}>
+					<TextArea rows={4} value={value} onChange={handleChange} />
+				</Col>
+			</Row>
+			<Row type="flex" justify="center">
+				<Col span={20}>
+					<Text strong>UPPERCASE</Text>
+					<p>{value.toUpperCase()}</p>
+					<Text strong>lowercase</Text>
+					<p>{value.toLowerCase()}</p>
+					<Text strong>camelCase</Text>
+					<p>{camelCase(value)}</p>
+				</Col>
+			</Row>
 		</div>
 	);
 }
