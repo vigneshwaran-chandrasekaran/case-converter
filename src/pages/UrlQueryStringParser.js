02863380a32928d1a935e7affc164b72d8dd759d
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-03-25 19:04:28 +0530

    query string page started

--- src/pages/UrlQueryStringParser.js
+++ src/pages/UrlQueryStringParser.js
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input, Row, Col, Typography } from 'antd';
+import { lowerCase, camelCase, kebabCase, upperCase, snakeCase } from 'lodash';
+
+const { TextArea } = Input;
+const { Text } = Typography;
 
 export function UrlQueryStringParser() {
-	return <div>UrlQueryStringParser</div>;
+	const [value, setValue] = useState('sample text');
+
+	function handleChange(event) {
+		setValue(event.target.value);
+	}
+
+	return (
+		<div className="mt-30">
+			<Row type="flex" justify="center">
+				<Col span={20}>
+					<h3>Case Converter</h3>
+					<TextArea
+						rows={4}
+						value={value}
+						onChange={handleChange}
+						spellCheck={true}
+					/>
+				</Col>
+			</Row>
+			<Row type="flex" justify="center" className="mt-30">
+				<Col span={20}>
+					<Text strong>UPPERCASE</Text>
+					<p>{upperCase(value)}</p>
+					<Text strong>lowercase</Text>
+					<p>{lowerCase(value)}</p>
+					<Text strong>camelCase</Text>
+					<p>{camelCase(value)}</p>
+					<Text strong>kebab-case</Text>
+					<p>{kebabCase(value)}</p>
+					<Text strong>snake_case(underscores)</Text>
+					<p>{snakeCase(value)}</p>
+				</Col>
+			</Row>
+		</div>
+	);
 }
