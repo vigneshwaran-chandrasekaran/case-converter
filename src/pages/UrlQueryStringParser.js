915d44029cbfb1305b8f405458e247ee4bfd783f
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-03-27 22:31:46 +0530

    added basic 2 parse functions

--- src/pages/UrlQueryStringParser.js
+++ src/pages/UrlQueryStringParser.js
@@ -10,6 +10,52 @@ export function UrlQueryStringParser() {
 
 	function handleChange(event) {
 		setValue(event.target.value);
+		getJsonFromUrl(event.target.value);
+		getJsonFromUrl2(event.target.value);
+	}
+
+	function getJsonFromUrl(url) {
+		// if (!url) url = location.search;
+		var query = url.substr(1);
+		var result = {};
+		query.split('&').forEach(function(part) {
+			var item = part.split('=');
+			result[item[0]] = decodeURIComponent(item[1]);
+		});
+		console.log('result 1', result);
+		return result;
+	}
+
+	function getJsonFromUrl2(url) {
+		// if(!url) url = location.href;
+		var question = url.indexOf('?');
+		var hash = url.indexOf('#');
+		if (hash == -1 && question == -1) return {};
+		if (hash == -1) hash = url.length;
+		var query =
+			question == -1 || hash == question + 1
+				? url.substring(hash)
+				: url.substring(question + 1, hash);
+		var result = {};
+		query.split('&').forEach(function(part) {
+			if (!part) return;
+			part = part.split('+').join(' '); // replace every + with space, regexp-free version
+			var eq = part.indexOf('=');
+			var key = eq > -1 ? part.substr(0, eq) : part;
+			var val = eq > -1 ? decodeURIComponent(part.substr(eq + 1)) : '';
+			var from = key.indexOf('[');
+			if (from == -1) result[decodeURIComponent(key)] = val;
+			else {
+				var to = key.indexOf(']', from);
+				var index = decodeURIComponent(key.substring(from + 1, to));
+				key = decodeURIComponent(key.substring(0, from));
+				if (!result[key]) result[key] = [];
+				if (!index) result[key].push(val);
+				else result[key][index] = val;
+			}
+		});
+		console.log('result 2', result);
+		// return result;
 	}
 
 	return (
