1ca430d3d92b73e46fccec89003c609487900a07
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-03-27 22:43:30 +0530

    sidebar link added

--- src/router.js
+++ src/router.js
@@ -1,16 +1,51 @@
 import React from 'react';
-import { Layout } from 'antd';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import {
+	UploadOutlined,
+	UserOutlined,
+	VideoCameraOutlined,
+} from '@ant-design/icons';
+
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 import * as Pages from './pages';
 
 const { Header, Footer, Sider, Content } = Layout;
 
+const { SubMenu } = Menu;
+
 export default function Router() {
 	return (
 		<BrowserRouter>
 			<Layout style={{ minHeight: '100vh' }}>
-				<Sider>Sider</Sider>
+				<Sider
+					breakpoint="lg"
+					collapsedWidth="0"
+					onBreakpoint={broken => {
+						console.log(broken);
+					}}
+					onCollapse={(collapsed, type) => {
+						console.log(collapsed, type);
+					}}
+				>
+					<div className="logo" />
+					<Menu
+						theme="light"
+						mode="inline"
+						defaultSelectedKeys={['1']}
+					>
+						<Menu.Item key="1">
+							<Link to="/case-converter">Case converter</Link>
+							<span className="nav-text">Case converter</span>
+						</Menu.Item>
+						<Menu.Item key="2">
+							<Link to="/url-query-string-parser">
+								URL Parser
+							</Link>
+							<span className="nav-text">URL Parser</span>
+						</Menu.Item>
+					</Menu>
+				</Sider>
 				<Layout>
 					<Header>Header</Header>
 					<Content>
